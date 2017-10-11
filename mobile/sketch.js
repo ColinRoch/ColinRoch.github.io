7d201cab4b22345fcfa3cda25b1812b48ed6d9fa
ColinRoch <ColinRoch@users.noreply.github.com>
2017-10-11 12:16:54 -0500

    7

--- mobile/sketch.js
+++ mobile/sketch.js
@@ -14,13 +14,13 @@ function draw() {
 	background(255);
 	beginShape();
 	for (var i = 0; i < touches.length; i++) {
-		fill(100, 200, 100);
+		fill(0, 125, 255);
 		vertex(touches[i].x, touches[i].y);
 	}
 	endShape(CLOSE);
 	for (i = 0; i < touches.length; i++) {
 		strokeWeight(5);
-		fill(0, 125, 255);
+		fill(0, 0, 255);
 		ellipse(touches[i].x, touches[i].y, 50, 50);
 		strokeWeight(1);
 		fill(0);
