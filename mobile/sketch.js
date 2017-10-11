e8318fd99544e8026429a207c79d9f40c240372d
ColinRoch <ColinRoch@users.noreply.github.com>
2017-10-11 10:03:38 -0500

    yay8

--- mobile/sketch.js
+++ mobile/sketch.js
@@ -16,6 +16,9 @@ fill(51, 153, 255);
 	beginShape();
 	for (var i = 0; i < touches.length; i++) {
 		vertex(touches[i].x, touches[i].y);
+		textSize(40);
+    text(touches[0].x, 100, 100);
+    text(touches[0].y, 200, 100);
 	}
 	endShape(CLOSE);
 	fill(0,0,255);
@@ -24,10 +27,4 @@ fill(51, 153, 255);
 	}
 }
 
-function touchStarted() {
-  background(255);
- textSize(40);
- text(touches[0].x, 100, 100);
- text(touches[0].y, 200, 100);
-}
 
