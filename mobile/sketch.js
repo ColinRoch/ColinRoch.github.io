20a5694a6b5ad9f12db6841d85f00b2563d64deb
ColinRoch <ColinRoch@users.noreply.github.com>
2017-10-11 10:06:57 -0500

    yay9

--- mobile/sketch.js
+++ mobile/sketch.js
@@ -19,6 +19,10 @@ fill(51, 153, 255);
 		textSize(40);
     text(touches[0].x, 100, 100);
     text(touches[0].y, 200, 100);
+    text(touches[0].x, 300, 100);
+    text(touches[0].y, 400, 100);
+    text(touches[0].x, 500, 100);
+    text(touches[0].y, 600, 100);
 	}
 	endShape(CLOSE);
 	fill(0,0,255);
