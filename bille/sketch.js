e4a83d348cd4aff16b8862402b67b2199d333b5c
ColinRoch <ColinRoch@users.noreply.github.com>
2017-10-19 18:43:35 -0500

    ssss

--- bille/sketch.js
+++ bille/sketch.js
@@ -1,4 +1,4 @@
-var px, py, vx, vy, ax, ay, vMultiplier, bsizex, bsizey, value;
+var px, py, vx, vy, ax, ay, vMultiplier, bsizex, bsizey, value, pic;
 
 function preload() {
   img1 = loadImage("image/colin3.png");
@@ -17,13 +17,14 @@ function setup() {
   px = windowWidth / 2;
   py = windowHeight / 2;
   value = 250;
+  pic = img1;
   imageMode(CENTER);
 }
 
 function draw() {
   background(value);
   textSize(15);
-  text("Version", 5, 15);
+  text("Version 2", 5, 15);
   text("Rx: " + floor(rotationX), 5, 30);
   text("Ry: " + floor(rotationY), 5, 45);
   drawBille();
@@ -44,43 +45,27 @@ function drawBille() {
   //ellipse(px + 27, py - 27, 120);
   //fill(255,255,255);
   //ellipse(px + 45, py - 45, 50);
-  image(img1, px, py, bsizey, bsizex);
+  image(pic, px, py, bsizey, bsizex);
 
   if (px > windowWidth - bsizex / 2) {
     px = windowWidth - bsizex / 2;
     vx = -vx * 0.3;
-    img1 = img2;
-  } else if (px > windowWidth - bsizex / 2) {
-    px = windowWidth - bsizex / 2;
-    vx = -vx * 0.3;
-    img2 = img1;
-  }
+    pic = img1;
+  } 
   if (px < 0 + bsizex / 2) {
     px = 0 + bsizex / 2;
     vx = -vx * 0.3;
-    img1 = img2;
-  }  else if(px < 0 + bsizex / 2) {
-    px = 0 + bsizex / 2;
-    vx = -vx * 0.3;
-    img2 = img1;
+    pic = img1;
   }
   if (py > windowHeight - bsizex / 2) {
     py = windowHeight - bsizex / 2;
     vy = -vy * 0.3;
-    img1 = img2;
-  } else if (py > windowHeight - bsizex / 2) {
-    py = windowHeight - bsizex / 2;
-    vy = -vy * 0.3;
-    img2 = img1;
+    pic = img2;
   }
   if (py < 0 + bsizex / 2) {
     py = 0 + bsizex / 2;
     vy = -vy * 0.3;
-    img1 = img2;
-  } else if (py < 0 + bsizex / 2) {
-    py = 0 + bsizex / 2;
-    vy = -vy * 0.3;
-    img2 = img1;
+    pic = img2;
   }
 }
 
